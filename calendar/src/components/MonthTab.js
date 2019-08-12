53c9c59b358a11247d1e3145e201bc4685ce7c2c
roy <roy>
2019-08-12 11:23:36 +0800

    Basic CSS edition2

--- calendar/src/components/MonthTab.js
+++ calendar/src/components/MonthTab.js
@@ -9,6 +9,10 @@ class MonthTab extends React.Component {
 
   render() {
     // console.log(this.props)
+    const nowMonth = moment(
+      this.props.MonthTabPack.initYearMonth,
+      'YYYYMM'
+    ).format('YYYY  M月')
     const leftMonth = moment(this.props.MonthTabPack.initYearMonth, 'YYYYMM')
       .add(-1, 'month')
       .format('YYYY  M月')
@@ -23,21 +27,27 @@ class MonthTab extends React.Component {
       <>
         <div
           onClick={this.props.MonthTabPack.monthswitchLeft}
-          className="d-flex jusifyCenter alignCenter"
+          className={'mothTabBg d-flex jusifyCenter alignCenter '}
         >
-          <span> {leftMonth}</span>
+          <span className="showmonth"> {leftMonth}</span>
+          <span className={nowMonth === leftMonth ? 'active' : ''}></span>
         </div>
         <div
           //   onClick={this.monthswitchMiddle}
-          className="d-flex jusifyCenter alignCenter"
+          className={'mothTabBg d-flex jusifyCenter alignCenter '}
         >
-          <span> {middleMonth}</span>
+          <span className="showmonth"> {middleMonth}</span>
+          <span className={nowMonth === middleMonth ? 'active' : ''}></span>
         </div>
         <div
           onClick={this.props.MonthTabPack.monthswitchRight}
-          className="d-flex jusifyCenter alignCenter"
+          className={
+            'd-flex jusifyCenter alignCenter ' +
+            (nowMonth === rightMonth ? 'active' : '')
+          }
         >
-          <span> {rightMonth}</span>
+          <span className="showmonth"> {rightMonth}</span>
+          <span className={nowMonth === rightMonth ? 'active' : ''}></span>
         </div>
       </>
     )
